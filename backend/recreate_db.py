1291c2be86af1f5be61db4089c28d0d9b081610b
adebolaadebisi <ayomikundebolaodefemi@gmail.com>
2025-11-24 15:39:49 +0100

    project

--- backend/recreate_db.py
+++ backend/recreate_db.py
@@ -1,7 +1,7 @@
 from database import Base, engine
 from models import Message
 
-# Drop and recreate all tables
+
 Base.metadata.drop_all(bind=engine)
 Base.metadata.create_all(bind=engine)
 print("Tables recreated successfully!")
